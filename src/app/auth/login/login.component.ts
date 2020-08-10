e45982c63c6a78cc8be33609f87aeb38b0cb56ef
Nassar setti <Abdennasser.Essati@4d.com>
2020-08-10 16:40:43 +0100

    chore: static renerding

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -20,16 +20,157 @@ export class LoginComponent implements OnInit {
 	}
 
 	login() {
-		this.authService.login(this.username, this.password).subscribe(
-			(result: IUser) => {
-				this.authService.user = result;
-				localStorage.setItem('auth-token', this.authService.user.token);
-				localStorage.setItem('current-user', JSON.stringify(this.authService.user));
-				this.router.navigate(['/home']);
-			},
-			error => {
-				alert('Oups something went wrong');
-			});
+		let result ;
+		if (this.username === 'user') {
+			result = {
+				"id": 1,
+				"nom": "Utilisateur",
+				"prenom": "user",
+				"login": "user",
+				"password": "user",
+				"passwordinbyte": null,
+				"profils": [
+					{
+						"id": 2,
+						"name": "Utilisateur",
+						"privileges": [
+							{
+								"id": 3,
+								"name": "Projet"
+							},
+							{
+								"id": 4,
+								"name": "Env"
+							}
+						]
+					}
+				],
+				"datecreation": null,
+				"mail": "user@test.com",
+				"tel": "06613444444",
+				"token": "usertoken"
+			}
+		} else {
+			result = {
+				"id": 1,
+				"nom": "administrateur",
+				"prenom": "admin",
+				"login": "admin",
+				"password": "admin",
+				"passwordinbyte": null,
+				"profils": [
+					{
+						"id": 1,
+						"name": "Administrateur",
+						"privileges": [
+							{
+								"id": 1,
+								"name": "Utilisateur"
+							},
+							{
+								"id": 2,
+								"name": "Profil"
+							},
+							{
+								"id": 3,
+								"name": "Projet"
+							},
+							{
+								"id": 4,
+								"name": "Env"
+							}
+						]
+					}
+				],
+				"datecreation": null,
+				"mail": "admin@test.com",
+				"tel": "06613444444",
+				"token": "admintoken"
+			}
+		}
+		
+		this.authService.user = result;
+		localStorage.setItem('auth-token', this.authService.user.token);
+		localStorage.setItem('current-user', JSON.stringify(this.authService.user));
+		this.router.navigate(['/home']);
+		// this.authService.login(this.username, this.password).subscribe(
+		// 	(result: IUser) => {
+		// 		if (this.username === 'user') {
+		// 			result = {
+		// 				"id": 1,
+		// 				"nom": "administrateur",
+		// 				"prenom": "admin",
+		// 				"login": "admin",
+		// 				"password": "admin",
+		// 				"passwordinbyte": null,
+		// 				"profils": [
+		// 					{
+		// 						"id": 2,
+		// 						"name": "Utilisateur",
+		// 						"privileges": [
+		// 							{
+		// 								"id": 3,
+		// 								"name": "Projet"
+		// 							},
+		// 							{
+		// 								"id": 4,
+		// 								"name": "Env"
+		// 							}
+		// 						]
+		// 					}
+		// 				],
+		// 				"datecreation": null,
+		// 				"mail": "user@test.com",
+		// 				"tel": "06613444444",
+		// 				"token": "usertoken"
+		// 			}
+		// 		} else {
+		// 			result = {
+		// 				"id": 1,
+		// 				"nom": "administrateur",
+		// 				"prenom": "admin",
+		// 				"login": "admin",
+		// 				"password": "admin",
+		// 				"passwordinbyte": null,
+		// 				"profils": [
+		// 					{
+		// 						"id": 1,
+		// 						"name": "Administrateur",
+		// 						"privileges": [
+		// 							{
+		// 								"id": 1,
+		// 								"name": "Utilisateur"
+		// 							},
+		// 							{
+		// 								"id": 2,
+		// 								"name": "Profil"
+		// 							},
+		// 							{
+		// 								"id": 3,
+		// 								"name": "Projet"
+		// 							},
+		// 							{
+		// 								"id": 4,
+		// 								"name": "Env"
+		// 							}
+		// 						]
+		// 					}
+		// 				],
+		// 				"datecreation": null,
+		// 				"mail": "admin@test.com",
+		// 				"tel": "06613444444",
+		// 				"token": "admintoken"
+		// 			}
+		// 		}
+				
+		// 		this.authService.user = result;
+		// 		localStorage.setItem('auth-token', this.authService.user.token);
+		// 		localStorage.setItem('current-user', JSON.stringify(this.authService.user));
+		// 		this.router.navigate(['/home']);
+		// 	},
+		// 	error => {
+		// 		alert('Oups something went wrong');
+		// 	});
 	}
 
 }
